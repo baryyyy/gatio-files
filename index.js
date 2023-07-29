d7c4dcd29511ebece7061a9448f482c94570957c
Bar <bar.yissachar@gmail.com>
2023-07-29 13:40:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Gatio Loaded!")
+console.log("%c%s", "background: #1c64f6; color: #fff; font-size: 10px; font-weight:bold; padding: 5px 10px; border-radius: 3px", "Gatio - All In One App Loaded! 🗲");
+
+
